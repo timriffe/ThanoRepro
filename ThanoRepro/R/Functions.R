c694f2ee051188d75b1d1a7a75a49da2f20922a8
tim <tim.riffe@gmail.com>
2014-09-09 11:51:26 -0700

    got perfect matrix match. further edits in text, formula corrections

--- ThanoRepro/R/Functions.R
+++ ThanoRepro/R/Functions.R
@@ -210,12 +210,12 @@ rThanoCoale <- compiler::cmpfun(function(Fy, da, .a = .5:110.5, maxit = 1e2, tol
 Rmomentn <- compiler::cmpfun(function(fx,Lx,x,n=0){
             sum((x^n)*fx*Lx)
         })
-rLotkaCoale <- compiler::cmpfun(function(fx,Lx,x=.5:110.5, maxit = 1e2, tol = 1e-15){
+rLotkaCoale <- compiler::cmpfun(function(fx,Lx,x=.5:110.5, T.guess = 29, maxit = 1e2, tol = 1e-15){
             # from Coale, Ansley J. (1957) A New Method for Calculating Lotka's r- the Intrinsic Rate of Growth in a Stable Population.
             # Population Studies, Vol. 11 no. 1, pp 92-94
             R0 <- Rmomentn(fx,Lx,x,0)
             # first assuming a mean generation time of 29
-            ri <- log(R0)/29
+            ri <- log(R0)/T.guess 
             for (i in 1:maxit){ # 10 is more than enough!
                 deltai <- sum(exp(-ri*x)*fx*Lx)-1
                 # the mean generation time self-corrects 
--- ThanoRepro/R/rMatrixEqual.R
+++ ThanoRepro/R/rMatrixEqual.R
@@ -14,12 +14,20 @@ fa <- with(Data, Fxf[Year == 2000 & Code == "SWE"])
 # 1) use l(a) instead of L(a)
 # further make age strictly integer...
 la2 <- with(Data, lx[Year == 2000 & Code == "SWE"])
+
+L <- rbind(fa, cbind(diag(la2[2:111] / la2[1:110]), 0))
+
 # 2) force d(a) to entail l(a) and vice versa.
 da2 <- -diff(c(la2,0))
 a2      <- 0:110
-(rL2    <- rLotkaCoale(fa,la2,x=a2))
+#(rL2    <- rLotkaCoale(fa,la2,x=a2)
+(rL2 <- log(Re(eigen(L)$values[1])))
 # 5) recalc stable age and births using these 'clean' inputs
 ca2     <- getca(la2,rL2,a2)
+
+ca2 <- Re(eigen(L)$vectors[, 1])
+ca2 <- ca2/sum(ca2)
+
 ba2     <- fa*ca2 
 # 6) and convert these to thanatological equivalents *without*
 # staggering the d(a) vector.
@@ -29,16 +37,64 @@ ba2     <- fa*ca2
 .fy2    <- .by2 / .cy2
 # ok, now make simplified Leslie:
 
-L <- rbind(fa, cbind(diag(la2[2:111] / la2[1:110]), 0))
 Y       <- outer(da2, .fy2, "*") + rbind(cbind(0, diag(110)), 0)
 
-# 
+# wowsa
 log(Re(eigen(L)$values[1]))
 log(Re(eigen(Y)$values[1]))
 
-# 5th decimal. Can it be the close-out?
-
-# same first eigenvalue(roughly), but different first eigenvectors...
-# these are the stable age structures
+## 5th decimal. Can it be the close-out?
+#
+## same first eigenvalue(roughly), but different first eigenvectors...
+## these are the stable age structures
 plot(Re(eigen(L)$vectors[, 1]),type='l')
-lines(abs(Re(eigen(Y)$vectors[, 1])),col="green
+lines(abs(Re(eigen(Y)$vectors[, 1])),col="green")
+
+ca2L <- Re(eigen(L)$vectors[, 1])
+ca2L <- ca2/sum(ca2)
+#ca2L-ca2
+#
+## can I get it exactly on a 4x4 matrix?
+#
+#la3 <- c(1,.8,.4,.1)
+#da3 <- c(.2,.4,.3,.1)
+## fa3 <- c(0,1,.5,0)
+#fa3 <- c(0,1,.6,0)
+#a3 <- 0:3
+#L3 <- rbind(fa3, cbind(diag(la3[2:4] / la3[1:3]), 0))
+#log(Re(eigen(L3)$values[1]))
+#
+##(rL3    <- rLotkaCoale(fa3,la3,x=a3,T.guess=1.6,maxit = 1e3))
+##sum(exp(-a3*rL3)*fa3*la3
+#rL3      <- log(Re(eigen(L3)$values[1]))
+#ca3      <- getca(la3,rL3,a3)
+#ba3      <- fa3*ca3 
+#cy3      <- rowSums(Thano(ca3,da3,stagger=FALSE))
+#by3      <- rowSums(Thano(ba3,da3,stagger=FALSE))
+## these is the 'clean' stable thanatological fertility schedule
+#fy3      <- by3 / cy3
+#Y3       <- outer(da3, fy3, "*") + rbind(cbind(0, diag(3)), 0)
+#
+## interesting ... both agree about 0 when fa3 = c(0,1,.5,0)
+#log(Re(eigen(L3)$values[1]))
+#log(Re(eigen(Y3)$values[1]))
+
+da2
+M <- diag(da2)
+col(M)
+M <- matrix(da2[col(M)],ncol=ncol(M))
+M[lower.tri(M)] <- 0
+
+
+M <- matrix(0,ncol=length(da2),nrow=length(da2))
+M[upper.tri(M,TRUE)] <- 1
+day <- diag(1/la2) %*% M %*% diag(da2)
+plot(rowSums(day*ca2)-ca2)
+
+matplot(day %*% diag(ca2),type='l')
+
+# try igraph stuff
+install.packages("igraph")
+
+
+
