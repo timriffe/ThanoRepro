6a85377bfe26c0ebc4d7615729c93e14682590a0
triffe <triffe@demog.berkeley.edu>
2014-10-16 17:05:15 -0700

    made paper package. First iteration. Will place script-specific functions in here...

---
+++ ThanoRepro/R/RiffeFunctions/raw/build.R
@@ -0,0 +1,19 @@
+
+# for Tim, this will choke
+if (system("hostname",intern=TRUE)=="triffe-N80Vm"){
+  # if I'm on the laptop
+  setwd("/home/tim/git/ThanoRepro/ThanoRepro")
+} else {
+  # in that case I'm on Berkeley system, and other people in the dept can run this too
+  setwd(paste0("/data/commons/",system("whoami",intern=TRUE),"/git/ThanoRepro/ThanoRepro"))
+}
+
+
+# use devtools package for easy documentation and building.
+
+library(devtools)
+
+devtools::document("R/RiffeFunctions")
+#devtools::load_all("R/RiffeFunctions")
+
+
