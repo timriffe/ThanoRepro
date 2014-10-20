9c06775c91a1903834f1202a3c5dc04c48390ca0
triffe <triffe@demog.berkeley.edu>
2014-10-20 12:08:42 -0700

    script now uses paper package

--- ThanoRepro/R/Pyramids.R
+++ ThanoRepro/R/Pyramids.R
@@ -7,58 +7,20 @@ if (system("hostname",intern=TRUE)=="triffe-N80Vm"){
   setwd(paste0("/data/commons/",system("whoami",intern=TRUE),"/git/ThanoRepro/ThanoRepro"))
 }
 source("R/Functions.R")
-source("R/PlotFunctions.R")
+#source("R/PlotFunctions.R")
+#
+#
+#devtools::load_all("/home/triffe/git/Leaves/PlosOne/R/RiffeetalFunctions")
 
 
-devtools::load_all("/home/triffe/git/Leaves/PlosOne/R/RiffeetalFunctions")
 
-plotPyr <- function(XXX,x,y,Thano=TRUE){
-    N <- ncol(XXX[["Cm"]])
-    mgrab <- ifelse(Thano,"Tm","Cm")
-    fgrab <- ifelse(Thano, "Tf", "Cf")
-    cgrab <- ifelse(Thano, "Tcol", "Ccol")
-    for (i in N:1){
-        m <- rep(XXX[[mgrab]][, i], each = 2)
-        f <- rep(XXX[[fgrab]][, i], each = 2)
-        polygon(c(m, rev(f)) + x, XXX[["y"]] + y, border = NA, col = XXX[[cgrab]][i])
-    }
-    
-    PyramidOutline(abs(XXX[[mgrab]][, N]), XXX[[fgrab]][, N], 
-            x = x, y = y, scale = 1, border = gray(.1), lwd = .2)
-}
-
-# OK data object in order
-# transform to plot-specific data
-prepare4plot <- function(XXX, colsFun1, colsFun2){
-    Mf          <- XXX$Females
-    Mm          <- XXX$Males
-    TOT         <- sum(Mf) + sum(Mm)
-    
-    Het1f       <- apply(Mf ,2, aggN, N = 10) / TOT             # age with ey het
-    Het1m       <- apply(Mm ,2, aggN, N = 10) / TOT             # age with ey het
-    Het2f       <- apply(Mf ,1, aggN, N = 10) / TOT             # age with ey het
-    Het2m       <- apply(Mm ,1, aggN, N = 10) / TOT 
-    
-# cumulative sum for cleaner plotting
-    cumHet1f    <- t(apply(Het1f, 2, cumsum))
-    cumHet2f    <- t(apply(Het2f, 2, cumsum))
-    cumHet1m    <- -t(apply(Het1m, 2, cumsum))
-    cumHet2m    <- -t(apply(Het2m, 2, cumsum))
-    
-    cols1       <- colsFun1(ncol(cumHet1m))
-    cols2       <- colsFun2(ncol(cumHet1m))
-    y           <- c(0, rep(1:110, each = 2), 111)
-    y           <- c(y, rev(y))
-    list(Cf = cumHet1f, Tf = cumHet2f, Cm = cumHet1m, Tm = cumHet2m, Ccol = cols1, Tcol = cols2, y = y)
-}
-
-colsFun1        <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, "YlGnBu"), space = "Lab")
-colsFun2        <- grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(9, "YlGn")), space = "Lab")
+colsFun1  <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, "YlGnBu"), space = "Lab")
+colsFun2  <- grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(9, "YlGn")), space = "Lab")
 
 
-Data <- local(get(load("Data/DataAll.Rdata")))
-US2010 <- GetThanoMatrices(.Code = "USA", .Year = 2010, Data = Data)
-USMat <- prepare4plot(US2010,colsFun1,colsFun2)
+Data      <- local(get(load("Data/DataAll.Rdata")))
+US2010    <- GetThanoMatrices(.Code = "USA", .Year = 2010, Data = Data)
+USMat     <- prepare4plot(US2010,colsFun1,colsFun2)
 
 percent.labs <- c("1.0%","0.8%","0.6%","0.4%","0.2%","","0.2%","0.4%","0.6%","0.8%","1.0%")
 
@@ -122,23 +84,6 @@ dev.off()
 #text(-.0125,117,"Years left",pos=4,cex=.8)
 #dev.off()
 #
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #
